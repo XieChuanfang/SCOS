00925cb7ff2e44e648eeb0751b2978cef1117745
xiechuanfang <1094710374@qq.com>
2015-01-31 18:32:09 +0800

    1.0

---
+++ SCOS/src/ustc/edu/sse/esd/activity/SCOSEntry.java
@@ -0,0 +1,29 @@
+package ustc.edu.sse.esd.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 
+ * SCOS鍏ュ彛Activity. <br>
+ * 鏄剧ずSCOS鐨凩ogo鍥剧墖.
+ * <p>
+ * Copyright: Copyright (c) 2015-1-31 涓嬪崍4:23:25
+ * <p>
+ * Company: 涓浗绉戝鎶�湳澶у杞欢瀛﹂櫌
+ * <p>
+ * 
+ * @author moon
+ * @version 1.0.0
+ */
+public class SCOSEntry extends Activity {
+
+	/**
+	 * Activity鐢熷懡鍛ㄦ湡涓殑閲嶈鏂规硶
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.entry); // 鍔犺浇甯冨眬鏂囦欢
+	}
+}
